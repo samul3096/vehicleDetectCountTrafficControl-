23e7573991fe650432e9115ac2f40a954f440bb8
samul3096 <shadat.haque@northsouth.edu>
2020-03-08 14:37:37 +0600

    led on&off control task begin here

--- ardu_tk.py
+++ ardu_tk.py
@@ -1,1 +1,17 @@
 import serial
+
+ard_srl=serial.Serial('com6',9600)
+print ard_srl.readline()
+print ("Enter 1 to ON and 0 to OFF")
+
+while 1:
+    in_data=raw_input()
+    print "you entered", in_data
+
+    if(in_data=='1'):
+        ard_srl.write('1')
+        print ("LED ON")
+
+    if (in_data == '0'):
+        ard_srl.write('0')
+        print ("LED OFF")
